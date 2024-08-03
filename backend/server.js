7eaa926918603f98074f3e7d7c9436dee89942eb
Sidhant <sidhant26sood@gmail.com>
2024-08-03 15:55:58 +0530

    chore: Add .env to .gitignore and update dependencies, build some auth

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose
+  .connect("mongodb://localhost:27017/albumDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
